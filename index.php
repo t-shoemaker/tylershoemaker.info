bfcb0464f43f5de732713733a762a3507e0869ed
Tyler Shoemaker <69219954+t-shoemaker@users.noreply.github.com>
2025-02-28 11:54:17 -0500

    Bio tweak

--- index.php
+++ index.php
@@ -20,11 +20,11 @@
         <h3>About</h3>
         <p>Tyler Shoemaker is a Neukom Fellow at the <a
         href="https://neukom.dartmouth.edu" target="_blank">Neukom Institute
-        for Computational Science</a>. A digital humanist, he conducts research
-        on language technology, focusing on how methods in natural language
-        processing (NLP) crosscut the interpretive and theoretic frameworks of
-        literary and media studies. Prior to joining the Neukom Institute,
-        Tyler was a Postdoctoral Scholar affiliated with the <a
+        for Computational Science</a>. He conducts research on language
+        technology, focusing on how methods in natural language processing
+        (NLP) crosscut the interpretive and theoretic frameworks of literary
+        and media studies. Prior to joining the Neukom Institute, Tyler was a
+        Postdoctoral Scholar affiliated with the <a
         href="https://datalab.ucdavis.edu" target="_blank">UC Davis
         DataLab</a>. There, he developed NLP methods across a variety of
         research domains, ranging from early modern print to environmental and

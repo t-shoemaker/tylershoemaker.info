0587b0b906c88a47a1ff48cc716d6baea203b557
Tyler Shoemaker <69219954+t-shoemaker@users.noreply.github.com>
2025-06-04 10:29:25 -0400

    Change email contact

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,7 +1,8 @@
     <div id="sidebar" class="column">
         <h3>Contact</h3>
         <hr>
-        <p>Email: Tyler.G.Shoemaker [at] dartmouth [dot] edu</p>
+        <p>Email: <a
+        href="mailto:tshoemaker@tamu.edu">tshoemaker@tamu.edu</a></p>
         <p>GitHub: <a href="https://github.com/t-shoemaker"
         target="_blank">t-shoemaker</a><p>
     </div>
